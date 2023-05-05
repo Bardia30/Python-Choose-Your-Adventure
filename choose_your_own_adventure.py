71cf2205084c2bbe03eaa8bc4a81bffea4639ca5
Bardia30 <110236333+Bardia30@users.noreply.github.com>
2023-05-05 08:38:55 -0400

    Add files via upload

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,46 @@
+# Choose your own adventure
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+left_or_right = input("Do you want to turn left or right?type 'L' or 'R': ").lower()
+if left_or_right != "l":
+    print("YOU HAVE FALLEN INTO A HOLE. GAME OVER!")
+else:
+    swim_or_wait = input("do you want to swim or wait? type 'S' or 'W': ").lower()
+    if swim_or_wait != "w":
+        print("you are stuck in a whirlpool, GAME OVER")
+    else: 
+        door = input("now you are at 3 doors, Blue, Red, and Yellow, which one? \n type 'Y', 'R', or 'B': ").lower()
+        if door == "r":
+            print("Burned by fire, GAME OVER!")
+        elif door == "b":
+            print("eaten by beasts, GAME OVER!")
+        elif door == "y":
+            print("You Win!")
+        else:
+            print("GAME OVER!")
